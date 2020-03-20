65a976a2fdc0768cd2e89fd64dbcf9edcd003d7e
Elias Paulino <eliaspaulinoandrade@gmail.com>
2020-03-20 09:29:07 -0300

    added the AnyEpic type erasurer

---
+++ CombineRedux/AnyEpic.swift
@@ -0,0 +1,27 @@
+//
+//  AnyEpic.swift
+//  CombineRedux
+//
+//  Created by Elias Paulino on 20/03/20.
+//  Copyright © 2020 Elias Paulino. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+/// A type erasurer to the Epics. Use it to make a reference for a Epic.
+struct AnyEpic<StateType>: UntypedActionEpic {
+    private let recoveredActionFor: (AnyPublisher<Action, Never>,
+                                    @escaping StateGetter<StateType>,
+                                    @escaping StateGetter<StateType>) -> [AnyPublisher<Action, Never>]
+    
+    init<EpicType: UntypedActionEpic>(epic: EpicType) where StateType == EpicType.StateType {
+        recoveredActionFor = epic.untypedActionsPublishersFor
+    }
+    
+    func untypedActionsPublishersFor(actionPublisher publisher: AnyPublisher<Action, Never>,
+                   appStateGetter: @escaping StateGetter<StateType>,
+                   oldAppStateGetter: @escaping StateGetter<StateType>) -> [AnyPublisher<Action, Never>] {
+        return recoveredActionFor(publisher, appStateGetter, oldAppStateGetter)
+    }
+}
